a328565b4cf726f92fbc5185c21593c60095ed44
huhduhbuh <theoni_lim@dlsu.edu.ph>
2024-11-05 23:21:37 +0800

    main menu done

--- React/src/pages/MainMenu/MainMenu.js
+++ React/src/pages/MainMenu/MainMenu.js
@@ -3,19 +3,22 @@ import { Link } from 'react-router-dom';
 import Sidebar from '../_sidebar/Sidebar'
 import global from '../../global.module.css';
 import TempHeader from '../_header/TempHeader';
+import styles from './MainMenu.module.css';
 
 const MainMenu = () => {
     return (
       <div className={global.wrapper}>
         <Sidebar></Sidebar>
-        <div className={global.content}>
+        <div>
           <TempHeader></TempHeader>
-          <div>
-            <h1>Main Menu</h1>
-            <Link to="/SetDefaults"> Set Defaults </Link> <br></br>
-            <Link to="/SearchEmployee/ViewPayrollHistory"> View Payroll History </Link> <br></br>
-            <Link to="/SearchEmployee/CalculatePayroll"> Generate Employee Payroll </Link> <br></br>
-            <Link to="/"> Exit </Link> <br></br>
+          <div className={global.mainContent}>
+            <h1><span className={global.title}>MAIN MENU</span></h1>
+            <div className={styles.links}>
+              <Link to="/SetDefaults"> Set Defaults </Link> <br></br>
+              <Link to="/SearchEmployee/ViewPayrollHistory"> View Payroll History </Link> <br></br>
+              <Link to="/SearchEmployee/CalculatePayroll"> Generate Employee Payroll </Link> <br></br>
+              <Link to="/"> Exit </Link> <br></br>
+            </div>
           </div>
         </div>
       </div>
