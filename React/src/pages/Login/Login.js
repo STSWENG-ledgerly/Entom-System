38afafca043d4fc6aa6828d2b419531da4c77ba7
huhduhbuh <theoni_lim@dlsu.edu.ph>
2024-11-05 16:53:32 +0800

    login css

--- React/src/pages/Login/Login.js
+++ React/src/pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from './Login.module.css';
 
 const Login = () => {
   const [password, setPassword] = useState('');
@@ -15,15 +16,21 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input 
-        type="password" 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)} 
-        placeholder="Enter password" 
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className={styles.background}>
+      <div className={styles.rect}>
+        <text className={styles.prompt}>ENTER PASSWORD</text>
+        <form className={styles.formSection}onSubmit={handleSubmit}>
+          <input className={styles.passwordHolder}
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            placeholder="Admin Password" 
+          /> <br></br>
+          <button className={styles.submitButton} type="submit">LOGIN</button>
+        </form>
+      </div>
+    </div>
+    
   );
 };
 
