b86801664a322bad87e625e7f02f2edc6bc904b5
kkgamedev <kendikorpbusiness@gmail.com>
2024-11-10 03:19:53 +0800

    fin payroll calculation, edit & delete feature, payment creation, changed edit url, styles

--- React/src/pages/GeneratePayroll/GeneratePayroll.js
+++ React/src/pages/GeneratePayroll/GeneratePayroll.js
@@ -6,11 +6,15 @@ import DeductionsInfo from '../_deductionsInfo/DeductionsInfo';
 import styles from './GeneratePayroll.module.css'
 import global from '../../global.module.css'
 import Header from '../_header/Header';
-import {ConfigContext} from '../../ConfigContext';
+import { ConfigContext } from '../../ConfigContext';
 
+//Audrey
 const GeneratePayroll = () => {
     const { id, fname, lname } = useParams();
-    //vars
+    const { config, createUserPayment } = useContext(ConfigContext);
+    const [showResults, setShowResults] = useState(false);
+    const [placeholderFile, setPlaceholderFile] = useState(null);
+
     const [payrollInfo, setPayrollInfo] = useState({
         date: '',
         ot: 0,
@@ -21,6 +25,9 @@ const GeneratePayroll = () => {
         otherPayrollInfo: 0
     });
     const [deductions, setDeductions] = useState({
+        sss: 0.,
+        philhealth: 0,
+        pagibig: 0,
         cashAdvance: 0,
         healthCard: 0,
         absences: 0,
@@ -31,9 +38,6 @@ const GeneratePayroll = () => {
         deductions: 0,
         total: 0
     });
-    const {config, setConfig} = useContext(ConfigContext);
-
-    const [placeholderFile, setPlaceholderFile] = useState(null);
 
     const generateEmail = () => {
         const emailContent = "test email";
@@ -50,73 +54,87 @@ const GeneratePayroll = () => {
         const encodedEmail = encodeURIComponent(hardcodedEmail);
         const encodedSubject = encodeURIComponent(subject);
         const encodedBody = encodeURIComponent(body);
-    
+
         const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodedEmail}&su=${encodedSubject}&body=${encodedBody}`;
         window.open(gmailUrl, '_blank');
     };
-    
+
     const calculatePayroll = () => {
         const { ot, salaryIncrease, mealAllow, bdayBonus, incentive, otherPayrollInfo } = payrollInfo;
-        const { cashAdvance, healthCard, absences, otherDeductions } = deductions;
-        const payroll_total = parseFloat(ot) + parseFloat(salaryIncrease) + parseFloat(mealAllow) +
-                        parseFloat(bdayBonus) + parseFloat(incentive) + parseFloat(otherPayrollInfo);
-        const deductions_total = parseFloat(cashAdvance) + parseFloat(healthCard) +
-                        parseFloat(absences) + parseFloat(otherDeductions);
+        const { sss, philhealth, pagibig, cashAdvance, healthCard, absences, otherDeductions } = deductions;
+        const payroll_total = parseFloat(ot * config.rate) + parseFloat(salaryIncrease) + parseFloat(mealAllow) +
+            parseFloat(bdayBonus) + parseFloat(incentive) + parseFloat(otherPayrollInfo)
+            + parseFloat(config.basic);
+        const deductions_total = parseFloat(sss) + parseFloat(philhealth) + parseFloat(pagibig) + parseFloat(cashAdvance) + parseFloat(healthCard) +
+            parseFloat(absences) + parseFloat(otherDeductions);
         const total = payroll_total - deductions_total;
 
         setResults({ payroll: payroll_total, deductions: deductions_total, total: total });
+        setShowResults(true);
+    };
+
+    const createUserPaymentData = () => {
+        const newData = { payrollInfo, deductions };
+        createUserPayment(id, newData);
     };
 
     return (
         <div className={global.wrapper}>
             <Sidebar></Sidebar>
             <div>
-            <Header></Header>
+                <Header></Header>
+                <div className={`${styles.container} ${global.mainContent}`}>
+                    <h1><span className={global.title}>Generate Payroll for {fname} {lname}</span></h1>
 
-        <div className={`${styles.container} ${global.mainContent}`}>
-            <h1><span className={global.title}>Generate Payroll for {fname} {lname}</span></h1>
+                    <div className={styles.box}>
+                        <span id={styles.infoTitle}>INPUT ALL THE INFORMATION NEEDED</span>
+                        <div className={styles.infoSection}>
+                            <div className={styles.formsSection}>
+                                <PayrollInfo payrollInfo={payrollInfo} setPayrollInfo={setPayrollInfo} />
+                                <DeductionsInfo deductions={deductions} setDeductions={setDeductions} />
+                                {!showResults && (
+                                    <div className={styles.buttonContainer}>
+                                        <button className={styles.button} onClick={() => { calculatePayroll(); createUserPaymentData();}}>
+                                            CALCULATE
+                                        </button>
+                                    </div>
+                                )}
+                            </div>
 
-                {/* demo on how to get and use the default configs
-                            <span>{config.rate}</span>
-                            <span>{config.basic}</span>
-                 */}
 
-            <PayrollInfo payrollInfo={payrollInfo} setPayrollInfo={setPayrollInfo} />
-            <DeductionsInfo deductions={deductions} setDeductions={setDeductions} />
-            <div className={styles.buttonContainer}>
-                <button className={styles.button} onClick={calculatePayroll}>Calculate</button>
-            </div>
-            
-            <div className={styles.resultSection}>
-                <p>Results</p>
-                <div>
-                    <label>Payroll: </label>
-                    <span>{results.payroll.toFixed(2)}</span>
-                </div>
-                <div>
-                    <label>Deductions: </label>
-                    <span>{results.deductions.toFixed(2)}</span>
-                </div>
-                <div>
-                    <label>Total: </label>
-                    <span>{results.total.toFixed(2)}</span>
-                </div>
-                <div className={styles.buttonContainer}>
-                    <button className={styles.button} onClick={generateEmail}>Email to Employee</button>
-                    {placeholderFile && (
-                        <div>
-                            <h3>Preview: </h3>
-                            <a href={placeholderFile} target="_blank" rel="noopener noreferrer">
-                            View Test File
-                            </a>
-                            <button onClick={sendEmail}>Email Test File</button>
+                            <div className={styles.resultSection}>
+                                {showResults && (
+                                    <div>
+                                        <div className={styles.formGroup}>
+                                            <label>PAYROLL: </label>
+                                            <input value={results.payroll.toFixed(2)} readOnly />
+                                        </div>
+                                        <div className={styles.formGroup}>
+                                            <label>DEDUCTIONS: </label>
+                                            <input value={results.deductions.toFixed(2)} readOnly />
+                                        </div>
+                                        <div className={styles.formGroup}>
+                                            <label>TOTAL: </label>
+                                            <input value={results.total.toFixed(2)} readOnly />
+                                        </div>
+                                        <div className={styles.buttonContainer}>
+                                            <button className={styles.button} onClick={generateEmail}>EMAIL TO EMPLOYEE</button>
+                                            {placeholderFile && (
+                                                <div>
+                                                    <span>Preview: </span>
+                                                    <a href={placeholderFile} target="_blank" rel="noopener noreferrer">
+                                                        View Test File
+                                                    </a>
+                                                    <button className={styles.button} onClick={sendEmail}>Email Test File</button>
+                                                </div>
+                                            )}
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
                         </div>
-                    )}
+                    </div>
                 </div>
-            </div>
-        </div>
-
-
             </div>
         </div>
     );
--- React/src/pages/ViewPayment/ViewPayment.js
+++ React/src/pages/ViewPayment/ViewPayment.js
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Sidebar from '../_sidebar/Sidebar';
 import Header from '../_header/Header';
 import global from '../../global.module.css';
-
+import { ConfigContext } from '../../ConfigContext';
 
 const ViewPayment = () => {
-    const navigate = useNavigate();
-    const { id, fname, lname } = useParams();  // params passed from previous pages
+  const navigate = useNavigate();
+  const { id, fname, lname } = useParams();  // params passed from previous pages
+  const { getAllUserPayments, deleteUserPayment } = useContext(ConfigContext);
+  const userPayments = getAllUserPayments(id);
 
-    const handleEdit = (payment_id) => {
-        navigate(`../EditPayroll/${payment_id}/${fname}/${lname}`);
-    };
+  const handleEdit = (payment_id) => {
+    navigate(`../EditPayroll/${id}/${payment_id}/${fname}/${lname}`);
+  };
 
-    const handleDelete = (payment_id) => {
-        // TODO - Audrey
-    };
+  const handleDelete = (payment_id) => {
+    deleteUserPayment(id, payment_id);
+  };
 
-    // TODO - Brian, pass each payment_id to respective buttons
-    return (
-      <div className={global.wrapper}>
-        <Sidebar></Sidebar>
-        <div>
+  // TODO - Brian, pass each payment_id to respective buttons
+  return (
+    <div className={global.wrapper}>
+      <Sidebar></Sidebar>
+      <div>
         <Header></Header>
 
         <div className={global.mainContent}>
-        <h1><span className={global.title}>Payroll History of {fname} {lname}</span></h1>
-        <td><button onClick={() => handleEdit(1)}>Edit</button></td>
-        <td><button onClick={() => handleDelete(1)}>Delete</button></td>
+          <h1><span className={global.title}>Payroll History of {fname} {lname}</span></h1>
+
+          {
+            //added payments here for navigation to edit payroll (Audrey)
+          }
+          {userPayments.map((payment) => (
+            <span key={payment.paymentId}>
+              <span>{payment.paymentId}</span>
+              <span>
+                <button onClick={() => handleEdit(payment.paymentId)}>Edit</button>
+                <button onClick={() => handleDelete(payment.paymentId)}>Delete</button>
+              </span>
+            </span>
+          ))}
+
+          {/*
+          <td><button onClick={() => handleEdit(1)}>Edit</button></td>
+          <td><button onClick={() => handleDelete(1)}>Delete</button></td>*/
+          }
         </div>
 
-        </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
+
 export default ViewPayment;
