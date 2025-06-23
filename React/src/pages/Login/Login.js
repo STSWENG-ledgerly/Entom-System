02b926faa807055a62cc8850743696516f9812d1
edg <122878019+sadhubby@users.noreply.github.com>
2025-06-24 00:18:01 +0800

    feat: changed design of login

--- React/src/pages/Login/Login.js
+++ React/src/pages/Login/Login.js
@@ -2,6 +2,8 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Login.module.css';
 import { ConfigContext } from '../../ConfigContext';
+import officeImage from '../Login/office.jpg';
+
 
 const Login = () => {
   const [userName, setUsername] = useState('');
@@ -38,23 +40,28 @@ const Login = () => {
   return (
     <div className={styles.background}>
       <div className={styles.rect}>
-        <text className={styles.prompt}>bruh</text>
-        <form className={styles.formSection} onSubmit={handleSubmit}>
-          <input className={styles.usernameHolder}
-            type="text"
-            value={userName}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-          />
-          <input className={styles.passwordHolder}
-            type="password"
-            value={userPassword}
-            onChange={(e) => setUserPassword(e.target.value)}
-            placeholder="Admin Password"
-          />
-          <span className={styles.errMessage}>{errMessage}</span><br></br>
-          <button className={styles.submitButton} type="submit">LOGIN</button>
-        </form>
+        <div className={styles.leftrect}>
+          <img src={officeImage} alt='bruh'></img>
+        </div>
+        <div className={styles.rightrect}>
+          <text className={styles.prompt}>Login to your system</text>
+          <form className={styles.formSection} onSubmit={handleSubmit}>
+            <input className={styles.usernameHolder}
+              type="text"
+              value={userName}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+            />
+            <input className={styles.passwordHolder}
+              type="password"
+              value={userPassword}
+              onChange={(e) => setUserPassword(e.target.value)}
+              placeholder="Admin Password"
+            />
+            <span className={styles.errMessage}>{errMessage}</span><br></br>
+            <button className={styles.submitButton} type="submit">LOGIN</button>
+          </form>
+        </div>
       </div>
     </div>
 
