926e1e644f14260a0191160fb11cee35fca0002b
WhoGoesOnMercury <rafael_hugo@dlsu.edu.ph>
2025-07-06 00:58:38 +0800

    Setup some Jest tests

--- React/src/setupTests.js
+++ React/src/setupTests.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
@@ -22,10 +21,3 @@ HTMLCanvasElement.prototype.getContext = () => {
     closePath: () => {},
   };
 };
-=======
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
->>>>>>> 82f56cebc60141cc4c75c50db7eec4b7ceaa8dd7
